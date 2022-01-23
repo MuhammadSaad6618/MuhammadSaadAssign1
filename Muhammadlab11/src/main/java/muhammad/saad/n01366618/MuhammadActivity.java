683a9adefc31ba3db6c5e7d05b024a8125e62d80
MuhammadSaad6618 <muhammadsaad.1274@gmail.com>
2022-01-22 19:13:17 -0500

    First commit of this assignment

---
+++ Muhammadlab11/src/main/java/muhammad/saad/n01366618/MuhammadActivity.java
@@ -0,0 +1,20 @@
+/*
+Name: Muhammad Saad
+Student#: n01366618
+section: RNA
+Assignment 1
+   */
+package muhammad.saad.n01366618;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MuhammadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Muhammadlab11/src/main/java/muhammad/saad/n01366618/SaadActivity.java
@@ -0,0 +1,14 @@
+package muhammad.saad.n01366618;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saad);
+    }
+}
